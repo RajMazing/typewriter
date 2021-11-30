0bda78eeb755ffa1669163de1d51d42f1116d246
RajMazing <caseyraj92@gmail.com>
2021-11-30 23:51:08 +0000

    updated code from vertical output to correct output with new line

--- typewriter.js
+++ typewriter.js
@@ -1,16 +1,24 @@
+
+
 const sentence = "hello there from lighthouse labs";
 let iterate  = 0;
-let speedster = 50;
+let speedster = 200; // changed it from 50 to 200
 
 
 
   for (const char of sentence) {
     setTimeout(() => {
-    process.stdout.write(`${char}\n`);
+    process.stdout.write(`${char}`);
    }, iterate);
    iterate += speedster;
   }
+  setTimeout(() => {
+    process.stdout.write('\n');
+  }, iterate);
    
+
+
+
 
 
 
